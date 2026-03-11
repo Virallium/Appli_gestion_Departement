6d26713b8b689e84d6ac879c6e2272bd7b489825
Miradi <mengimiradit@gmail.com>
2026-03-11 23:23:13 +0100

    section_politique_de_confifentialite

--- AuthentificationApp/models.py
+++ AuthentificationApp/models.py
@@ -8,7 +8,8 @@ class Utilisateur(models.Model):
     numero = models.CharField(max_length=20, verbose_name='Numéro de téléphone', default='0901717545')
     email = models.EmailField(verbose_name='Adresse e-mail',default='mengimiradi@gmail.com')
     motdepasse = models.CharField(max_length=100, verbose_name="Mot de passe", default='Miradi000@')
+    adresse=models.CharField(max_length=200, verbose_name='Adresse', default='51, Kisantu-Kinshasa, Selembao')
 
     def __str__(self):
-        return f"{self.nom} {self.posnom} {self.prenom}",
+        return f"{self.nom} {self.postnom} {self.prenom} {self.adresse}",
 
