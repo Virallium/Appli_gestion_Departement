f447d6d2bb2239456f1ddbb587ac30772fe76ef4
Miradi <mengimiradit@gmail.com>
2026-03-13 01:00:17 +0100

    section auth

--- AuthentificationApp/models.py
+++ AuthentificationApp/models.py
@@ -2,14 +2,16 @@ from django.db import models
 
 class Utilisateur(models.Model):
     nomUt=models.CharField(max_length=100,verbose_name="nom utilisateur",default='miradi')
-    nom = models.CharField(max_length=100, verbose_name='Nom', default='mengi')
     postnom = models.CharField(max_length=100, verbose_name='Postnom', default='ndingambote')
     prenom = models.CharField(max_length=100, verbose_name="Prénom", default='mengi')
-    numero = models.CharField(max_length=20, verbose_name='Numéro de téléphone', default='0901717545')
     email = models.EmailField(verbose_name='Adresse e-mail',default='mengimiradi@gmail.com')
     motdepasse = models.CharField(max_length=100, verbose_name="Mot de passe", default='Miradi000@')
-    adresse=models.CharField(max_length=200, verbose_name='Adresse', default='51, Kisantu-Kinshasa, Selembao')
+
 
     def __str__(self):
-        return f"{self.nom} {self.postnom} {self.prenom} {self.adresse}",
+        return f"{self.nomUt} {self.postnom} {self.prenom}"
 
+class profil(models.Model):
+    user=models.OneToOneField(Utilisateur,on_delete=models.CASCADE)
+    adresse=models.CharField(max_length=200, verbose_name='Adresse', default='51, Kisantu-Kinshasa, Selembao')
+    numero = models.CharField(max_length=20, verbose_name='Numéro de téléphone', default='0901717545')
--- AuthentificationApp/views.py
+++ AuthentificationApp/views.py
@@ -1,9 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import profil
+from django.contrib import messages
+from django.contrib.auth.models import User 
+from django.contrib.auth import authenticate, login, logout
+def register_view(request):
+    if request.method=="POST":
+        nom_Utilisateur=request.POST['name']
+        postnom=request.POST['postname']
+        prenom=request.POST['pren']
+        telephone=request.POST['num']
+        mail=request.POST['mail']
+        Adresse=request.POST['Ad']
+        mpass=request.POST['pasword']
+        mon_Utilisateur=User.objects.create_user(username=nom_Utilisateur,email=mail,password=mpass)
+        mon_Utilisateur.first_name=prenom
+        mon_Utilisateur.last_name=postnom
+        mon_Utilisateur.email=mail
+        mon_Utilisateur.username=nom_Utilisateur
+        profil.objects.create(adresse=Adresse,numero=telephone)
+        mon_Utilisateur.save()
+        messages.success(request,'Votre compte a été crée avec succèes')
+        return redirect('login')
+    return render(request, 'auth/register.html')
+
 def login_view(request):
+    if request.method=="POST":
+        nom_Utilisateur=request.POST['name']
+        mpass=request.POST['pasword']
+        user=authenticate(username=nom_Utilisateur, password=mpass)
+        if user is not None:
+            login(request,user)
+            messages.success(request,'Vous vous ète connecté')
+            return redirect('Accueil')
+        else:
+            messages.error(request,'Nom d\'utilisateur ou mot de passe incorrect')
+            return redirect('login')
     return render(request, 'auth/login.html')
 
-def register_view(request):
-    return render(request, 'auth/register.html')
 def accueil_view(request):
     return render(request, 'pages/index.html')
 def Activites(request):
