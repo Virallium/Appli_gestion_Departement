49fdb9408d5a33bd590df46b6dd269091c9c9ca9
Virallium <mengimiradit@gmail.com>
2026-07-10 00:56:31 +0200

    j'ai changé la base de données

--- AccueilnouveauProject/settings.py
+++ AccueilnouveauProject/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 from decouple import config
 from dotenv import load_dotenv
-load_env()
+load_dotenv()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
